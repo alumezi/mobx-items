7393abdb483d57cc2ec2dd797ee1923d382fa500
alumezi <alumezi95@gmail.com>
2019-02-20 15:37:02 +0100

    using observer to access store is deprecated, introducing inject

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { observer } from 'mobx-react'
+import { observer, inject } from 'mobx-react'
 
-const AddItem = observer(['store'], ({ store }) => {
-  return (
-    <form onSubmit={store.submitNewTodo}>
-      <input
-        type='text'
-        onChange={store.changeNewItemInput}
-        value={store.newItemInput}
-      />
-      <input type='submit' value='Submit' />
-    </form>
-  )
-})
+const AddItem = inject('store')(
+  observer(({ store }) => {
+    return (
+      <form onSubmit={store.submitNewTodo}>
+        <input
+          type='text'
+          onChange={store.changeNewItemInput}
+          value={store.newItemInput}
+        />
+        <input type='submit' value='Submit' />
+      </form>
+    )
+  })
+)
 
 export default AddItem
